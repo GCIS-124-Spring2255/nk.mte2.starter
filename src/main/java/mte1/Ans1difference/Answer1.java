e34edb1deebb31080b81c7025e527e2cb4a17c82
NareshKshetri <nxkics@rit.edu>
2026-02-28 15:20:57 -0500

    Initial commit

---
+++ src/main/java/mte1/Ans1difference/Answer1.java
@@ -0,0 +1,22 @@
+package mte1.Ans1difference;
+
+/* 
+Q1 [30 points]. Given some key differences between Python and Java, choose the correct answer between the two high level programming languages (Python / Java) 
+and display all answers in a nicely formatted output in a Java file (inside the difference package) named Answer1.java. 
+1.1 Which programming language is NOT a great OO language? 
+1.2 Which programming language is a COMPILED programming language? 
+1.3 Which language requires a bit of SCAFFOLDING to write even basic programs? 
+1.4 Which programming language can declare a variable WITHOUT defining a variable type? 
+1.5 Which programming language uses “extends” to establish an inheritance relationship with a parent class? 
+1.6 Which programming language doesn’t SUPPORT the power operator (**) and integer division operator (//) ? 
+1.7 Which programming language SUPPORTS single inheritance with respect to classes (extends at most 1 other class) ?
+1.8 Which programming language CANNOT define a method without implementing it (by making it an abstract method) ? 
+1.9 Which programming language has (when object created) all attributes ALLOCATED in memory, assigned default values ? 
+1.10 Which programming language guarantee object to ASSIGN value to any fields defined whether we write a constructor/not ?
+*/
+
+public class Answer1 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Answer 1 file");
+    }
+}
